1173b482df86668c6b1f484bbf27bcff971df09b
PatelDarshil <darshil.patel@stridelysolutions.com>
2019-10-17 23:42:16 +0530

    Modified data type for demo

--- Tables/dbo.tbl_User.sql
+++ Tables/dbo.tbl_User.sql
@@ -5,7 +5,7 @@ CREATE TABLE [dbo].[tbl_User]
 [EmailID] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ContactNo] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [IsActive] [bit] NOT NULL,
-[IsDeleted] [bit] NOT NULL
+[IsDeleted] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[tbl_User] ADD CONSTRAINT [PK_tbl_User] PRIMARY KEY CLUSTERED  ([UserID]) ON [PRIMARY]
