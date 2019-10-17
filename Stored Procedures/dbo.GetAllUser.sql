6f2404a05713d006d892cb4831506d484aa279b0
PatelDarshil <darshil.patel@stridelysolutions.com>
2019-10-17 12:48:45 +0530

    add

---
+++ Stored Procedures/dbo.GetAllUser.sql
@@ -0,0 +1,16 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllUser]
+	
+AS
+BEGIN
+SELECT * FROM dbo.tbl_User
+END
+GO
